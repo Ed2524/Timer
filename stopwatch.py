111f864cf92c96331b5a263770ed7ce54bc7ec79
Ed2524 <166954551+Ed2524@users.noreply.github.com>
2024-05-16 20:45:23 +0400

    Add files via upload

---
+++ stopwatch.py
@@ -0,0 +1,19 @@
+# import time 
+
+
+# def start_stopwATCH():
+#     start_time = time.time()
+#     input("Stopwatch started. Press enter to stop")
+#     end_time = time.time()
+#     verjnakan_time = end_time - start_time
+#     print(verjnakan_time)
+    
+# while True:
+#     print("\n Options:")
+#     print("1. Start StopWatch")
+#     print("2. exit")
+#     x = input("say number: 1 or 2 ")
+#     if x == "1":
+#         start_stopwATCH()
+#     else:
+#         break
